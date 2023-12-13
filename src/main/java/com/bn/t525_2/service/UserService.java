9d2d69d32b89d7538875dd515e62ca0d763df7d4
Benson Ning <111818511+Aomame-Masami@users.noreply.github.com>
2023-12-13 17:20:04 -0600

    Delete src directory

--- src/main/java/com/bn/t525_2/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.bn.t525_2.service;
-
-import com.bn.t525_2.pojo.User;
-import org.springframework.stereotype.Service;
-
-public interface UserService {
-
-    User login(String username, String password);
-}
